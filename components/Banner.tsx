0dcc6e14b93a8ada32b75d15ffddefe01a3598ac
tunew <tunew@LAPTOP-5JGL0CNA>
2022-05-30 23:21:35 +0300

    fix 2nd try

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import { baseUrl } from '../constants/movie'
 import { Movie } from '../typings'
-import {FaPlay} from "react-icons/fa"
+import { FaPlay } from 'react-icons/fa'
 import { InformationCircleIcon } from '@heroicons/react/solid'
 interface Props {
   netflixOriginals: Movie[]
@@ -15,7 +15,6 @@ function Banner({ netflixOriginals }: Props) {
       netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
     )
   }, [netflixOriginals])
-  console.log(movie)
 
   return (
     <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
@@ -35,8 +34,12 @@ function Banner({ netflixOriginals }: Props) {
       </p>
 
       <div className="flex space-x-3">
-        <button className="bannerButton bg-white text-black"><FaPlay className="h-4 w-4 text-black md:h-7 md:w-7"/> Play</button>
-        <button className="bannerButton bg-[gray]/70">More Info <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8"/></button>
+        <button className="bannerButton bg-white text-black">
+          <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7" /> Play
+        </button>
+        <button className="bannerButton bg-[gray]/70">
+          More Info <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8" />
+        </button>
       </div>
     </div>
   )
