26e1b971978c6a11102c30dc3cf84132809a8b54
tunew <tunew@LAPTOP-5JGL0CNA>
2022-06-05 23:48:55 +0300

    at least..

--- utils/requests.ts
+++ utils/requests.ts
@@ -2,14 +2,14 @@ const API_KEY = process.env.NEXT_PUBLIC_API_KEY
 const BASE_URL = 'https://api.themoviedb.org/3'
 
 const requests = {
-  fetchTrending: `${BASE_URL}/trending/all/week?api_key=${API_KEY}&language=en-US`,
-  fetchNetflixOriginals: `${BASE_URL}/discover/movie?api_key=${API_KEY}&with_networks=213`,
-  fetchTopRated: `${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-  fetchActionMovies: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=28`,
-  fetchComedyMovies: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=35`,
-  fetchHorrorMovies: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=27`,
-  fetchRomanceMovies: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=10749`,
-  fetchDocumentaries: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=99`,
+  fetchTrending: `https://api.themoviedb.org/3/trending/all/week?api_key=528320c112c7f22f82b0627291c9153a&language=en-US`,
+  fetchNetflixOriginals: `https://api.themoviedb.org/3/discover/movie?api_key=528320c112c7f22f82b0627291c9153a&with_networks=213`,
+  fetchTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=528320c112c7f22f82b0627291c9153a&language=en-US`,
+  fetchActionMovies: `https://api.themoviedb.org/3/discover/movie?api_key=528320c112c7f22f82b0627291c9153a&language=en-US&with_genres=28`,
+  fetchComedyMovies: `https://api.themoviedb.org/3/discover/movie?api_key=528320c112c7f22f82b0627291c9153a&language=en-US&with_genres=35`,
+  fetchHorrorMovies: `https://api.themoviedb.org/3/discover/movie?api_key=528320c112c7f22f82b0627291c9153a&language=en-US&with_genres=27`,
+  fetchRomanceMovies: `https://api.themoviedb.org/3/discover/movie?api_key=528320c112c7f22f82b0627291c9153a&language=en-US&with_genres=10749`,
+  fetchDocumentaries: `https://api.themoviedb.org/3/discover/movie?api_key=528320c112c7f22f82b0627291c9153a&language=en-US&with_genres=99`,
 }
 
 export default requests
